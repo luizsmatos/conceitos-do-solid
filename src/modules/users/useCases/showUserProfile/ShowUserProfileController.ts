4054bd67f6b0553cfec93c23624d6a2c1a7d8ed8
luizsmatos <gusstavopdm@gmail.com>
2022-01-21 11:27:32 -0300

    feature: ShowUserProfileUseCase

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,12 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id: user_idRaw } = request.params;
+    const user_id = user_idRaw.toString();
+
+    this.showUserProfileUseCase.execute({ user_id });
+
+    return response.send();
   }
 }
 
