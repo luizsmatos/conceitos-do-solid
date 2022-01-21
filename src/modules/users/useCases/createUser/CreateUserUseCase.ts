1a7a744094c075ddd4a1bf1fb68a1c60b5baf105
luizsmatos <gusstavopdm@gmail.com>
2022-01-21 11:23:28 -0300

    feature: TurnUserAdminUseCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,7 @@ class CreateUserUseCase {
     const userAlreadyExists = this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error("");
+      throw new Error("User Already Exists");
     }
 
     const user = this.usersRepository.create({ name, email });
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,11 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id: user_idRaw } = request.params;
+    const user_id = user_idRaw.toString();
+
+    this.turnUserAdminUseCase.execute({ user_id });
+    return response.status(204).send();
   }
 }
 
